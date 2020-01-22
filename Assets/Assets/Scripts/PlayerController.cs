8ad7e8af83d6e308bae7ca471f7c531872a2b08b
NicholasMiceli <nicholas.miceli@knights.ucf.edu>
2020-01-21 20:41:25 -0500

    Created the prototype

---
+++ Assets/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    public Text scoreText;
+    public Text winText;
+    public Text loseText;
+    public float speed = 0.4f;
+    private int score;
+    Vector2 dest = Vector2.zero;
+
+     void Start() {
+         dest = transform.position;
+         score = 0;
+         winText.text = "";
+         loseText.text = "";
+         SetScoreText();
+     }
+
+     void FixedUpdate() {
+
+       if (Input.GetKeyDown(KeyCode.Escape)){
+            Application.Quit();}
+
+         Vector2 p = Vector2.MoveTowards(transform.position, dest, speed);
+         GetComponent<Rigidbody2D>().MovePosition(p);
+
+         if ((Vector2)transform.position == dest) {
+       if (Input.GetKey(KeyCode.UpArrow) && valid(Vector2.up))
+           dest = (Vector2)transform.position + Vector2.up;
+       if (Input.GetKey(KeyCode.RightArrow) && valid(Vector2.right))
+           dest = (Vector2)transform.position + Vector2.right;
+       if (Input.GetKey(KeyCode.DownArrow) && valid(-Vector2.up))
+           dest = (Vector2)transform.position - Vector2.up;
+       if (Input.GetKey(KeyCode.LeftArrow) && valid(-Vector2.right))
+           dest = (Vector2)transform.position - Vector2.right;
+   }
+     }
+
+     bool valid(Vector2 dir) {
+         Vector2 pos = transform.position;
+         RaycastHit2D hit = Physics2D.Linecast(pos + dir, pos);
+         return (hit.collider == GetComponent<Collider2D>());
+     }
+     void OnTriggerEnter2D(Collider2D other)
+         {
+             if (other.gameObject.CompareTag ("Pickup"))
+             {
+                 other.gameObject.SetActive (false);
+                 score = score + 10;
+                 SetScoreText();
+             }
+             if (other.gameObject.CompareTag("Enemy"))
+             {
+                 other.gameObject.SetActive (false);
+
+             }
+        }
+
+        void SetScoreText()
+        {
+        scoreText.text = "Score: " + score.ToString ();
+        if (score >= 100)
+        {
+            winText.text = "You Win!";
+        }
+    }
+}
